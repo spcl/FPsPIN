f5feca170a84c9236edf4ecd1d34c23261d7787c
Timo Schneider <timos@inf.ethz.ch>
2024-05-26 00:38:33 +0200

    fix pp plot scripts

--- fpga/app/pspin/deps/pspin/examples/icmp_ping/timo_pp_plot.py
+++ fpga/app/pspin/deps/pspin/examples/icmp_ping/timo_pp_plot.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 sns.set_style("whitegrid")
 data = pd.read_csv("timo_pp_data.dat", header=None, sep="\s+")
 data = data.rename(columns={0: "Protocol", 1: "Mode", 2: "Size", 3: "Time"})
+print(data)
 g = sns.FacetGrid(data=data, col="Protocol")
 g.map_dataframe(sns.lineplot, x="Size", y="Time", errorbar="ci", estimator="median", err_style="bars", hue="Mode")
 g.set_titles(col_template="{col_name} Ping-Pong", row_template="{row_name}")
